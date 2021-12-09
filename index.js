07a2c9f8406f0522874247ab1e6b5e64d4eaa425
jordan Smith <Jordan@umbrage.com>
2021-12-09 14:33:04 -0600

    feat(apollo) init graph ql

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 require("dotenv").config();
 const { PORT } = process.env;
 const express = require("express");
-const app = express();
 var emoji = require("node-emoji");
 const { Sequelize } = require("sequelize");
 const config = require("./config/config");
-var models = require("./models");
+const { ApolloServer, gql } = require("apollo-server-express");
+
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: "postgres",
 });
 
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
 app.listen(PORT, async () => {
+  await server.start();
+  await server.applyMiddleware({ app });
   await sequelize
     .authenticate()
     .then(() => {
       console.log(`${emoji.get("white_check_mark")} Connected to the database.`);
     })
     .catch((error) => {
-      console.log(`${emoji.get("middle_finger")}========== Failed to connect to the database ===========${emoji.get("middle_finger")} `);
+      console.log(`${emoji.get("x")}========== Failed to connect to the database ===========${emoji.get("x")} `);
       console.log(error);
-      console.log(`${emoji.get("middle_finger")}========== Failed to connect to the database ===========${emoji.get("middle_finger")} `);
+      console.log(`${emoji.get("x")}========== Failed to connect to the database ===========${emoji.get("x")} `);
     });
 
-  console.log(`${emoji.get("rocket")} Running on port ${PORT}`);
-
-  const contracts = await models.Contracts.findAll();
-  console.log(contracts);
+  console.log(`${emoji.get("rocket")} Running on http://localhost:${process.env.PORT}${server.graphqlPath}`);
 });
